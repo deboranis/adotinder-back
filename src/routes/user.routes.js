b5562ac54075d2a5d3e68a4a1d9b06a04e15c134
Débora Nisenbaum <dc.nisen@hotmail.com>
2021-06-25 00:46:13 -0300

    added protetor pets route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { getUserPets } from '../controller/pet.controller';
 import { getUser, editUser, deleteUser } from '../controller/user.controller';
 import AppError from '../errors/AppError';
 import { validate } from '../utils/authManager';
@@ -6,36 +7,44 @@ import { validate } from '../utils/authManager';
 const router = Router();
 
 router.get('/edit', async (request, response) => {
-  try {
-    const { token } = request.signedCookies;
-    const id = validate(token);
-    const user = await getUser(id.id)
-    response.status(200).json(user);
-  } catch (error) {
-    response.status(error.status).json(new AppError(error));
-  }
+	try {
+		const { token } = request.signedCookies;
+		const id = validate(token);
+		const user = await getUser(id.id);
+		response.status(200).json(user);
+	} catch (error) {
+		response.status(error.status).json(new AppError(error));
+	}
 });
 
 router.post('/edit', async (request, response) => {
-  try {
-    const { body } = request;
-    const { id } = validate(request.signedCookies.token);
-    const editedUser = editUser(id, body);
-    response.status(200).json(editedUser);
-  } catch (error) {
-    response.status(error.status).json(new AppError(error));
-  }
+	try {
+		const { body } = request;
+		const { id } = validate(request.signedCookies.token);
+		const editedUser = editUser(id, body);
+		response.status(200).json(editedUser);
+	} catch (error) {
+		response.status(error.status).json(new AppError(error));
+	}
 });
 
 router.delete('/delete', async (request, response) => {
-  try {
-    const { id } = validate(request.signedCookies.token); //como essa função é síncrona, vai retornar um objeto que por sua vez tem prop id
-    await deleteUser(id);
-    response.clearCookie('token').status(200).json({ message: 'Usuário removido com sucesso.' });
-  } catch (error) {
-    response.status(error.status).json(new AppError(error));
-  }
+	try {
+		const { id } = validate(request.signedCookies.token); // como essa função é síncrona, vai retornar um objeto que por sua vez tem prop id
+		await deleteUser(id);
+		response.clearCookie('token').status(200).json({ message: 'Usuário removido com sucesso.' });
+	} catch (error) {
+		response.status(error.status).json(new AppError(error));
+	}
+});
+
+router.get('/pets', async (request, response) => {
+	try {
+		const pets = await getUserPets(request.user.id);
+		response.status(200).json(pets);
+	} catch (error) {
+		response.status(500).json(new AppError(error));
+	}
 });
 
 export default router;
-
