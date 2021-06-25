3eea34d8e47a4c796ad4ed32c336986de8c43ced
Débora Nisenbaum <dc.nisen@hotmail.com>
2021-06-25 00:45:35 -0300

    added quiz route

--- src/routes/pets.routes.js
+++ src/routes/pets.routes.js
@@ -1,17 +1,25 @@
 import { Router } from 'express';
-import { createPet } from '../controller/pet.controller';
+import { createPet, getQuizPets } from '../controller/pet.controller';
 import AppError from '../errors/AppError';
 
 const router = Router();
 
 router.post('/criar', async (request, response) => {
-  try {  
-    await createPet({ ...request.body, protetor: request.user.id });
-    response.status(200).json({ message: 'Pet criado com sucesso!' });
-  } catch (error) {
-    console.log(error);
-    response.status(400).json(new AppError(error));
-  }
+	try {
+		await createPet({ ...request.body, protetor: request.user.id });
+		response.status(200).json({ message: 'Pet criado com sucesso!' });
+	} catch (error) {
+		response.status(400).json(new AppError(error));
+	}
+});
+
+router.get('/quiz', async (request, response) => {
+	try {
+		const quizResult = await getQuizPets({ ...request.query });
+		response.status(200).json(quizResult);
+	} catch (error) {
+		response.status(500).json(new AppError(error));
+	}
 });
 
 export default router;
